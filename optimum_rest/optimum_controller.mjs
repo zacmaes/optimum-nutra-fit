6405016042d84c7c1e7ab54ed98e7d78aa377ed0
zacmaes <zachary.maes@gmail.com>
2023-01-13 19:18:49 -0600

    controller hello world runs on port 3000, added nodemon

--- optimum_rest/optimum_controller.mjs
+++ optimum_rest/optimum_controller.mjs
@@ -1,1 +1,26 @@
-// controller
+import 'dotenv/config';
+import * as users from './optimum_model.mjs';
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+
+// add a body validation function here
+
+// add a date validation function here
+
+// hello world test
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
